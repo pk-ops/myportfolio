0680980ded48416f10619e6b6e15d005874b4f84
pk-ops <kambleprathamesh60@gmail.com>
2022-08-21 16:58:31 +0530

    work-updated

--- src/Work.js
+++ src/Work.js
@@ -1,7 +1,27 @@
 import React from "react";
-
+import "./work.css"
 export default function Work(){
-  return<>
-  Work
-   </>;
-}
+
+  return(
+ <div className="d-flex flex-column">
+    <div className="education-one my-4 justify-content-between 
+      d-flex flex-row">
+        <div className="d-flex flex-column new-one  ">
+          <span className="company-name">
+           <p>Experience</p> 
+            <span>Accenture</span>
+            </span>
+          
+           <span className="designation">
+           Application Development Associate (1yr+ Exp)
+          </span>
+        </div>
+        <div >
+          <span className="experience">
+            2021- present
+          </span>
+        </div>
+      </div>
+      </div>
+  
+)};
